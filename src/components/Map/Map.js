99c590200188dbc9e6d42f2cb2602558397b94f7
Ruby Pruneda <rubypruneda@outlook.com>
2018-03-28 09:01:00 -0600

    working

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios'
 
 class Map extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Map Page</h1>
-      </div>
-    );
-  }
+
+
+
+
+componentDidMount(){
+axios.get('/api/comments').then(resp=>{
+ console.log(resp)
+
+})
+ 
+}
+ render() {
+   return (
+     <div>
+       <h1>Map Page</h1>
+     </div>
+   );
+ }
 }
 
 export default Map;
